3fe2a621eed3b6d8b7b2de078972ac36ce0935ea
Christoph Lindemann <clin@foss.dk>
2020-01-08 07:37:51 +0100

    Clean proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NU5129.Build.Props.Problem
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return (args ?? Array.Empty<string>()).Length;
+        }
+    }
+}
